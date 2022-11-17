566d5bced23bfb2d27b57e3a20f228da0cf05d10
esaid <emmanuel.said@cern.ch>
2022-11-17 16:12:39 +0100

    tutoriel https://www.datacamp.com/tutorial/streamlit

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.write("Hello ,let's learn how to build a streamlit app together")
+st.title ("this is the app title")
+st.header("this is the markdown")
+st.markdown("this is the header")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+st.checkbox('yes')
+st.button('Click')
+st.radio('Pick your gender',['Male','Female'])
+st.selectbox('Pick your gender',['Male','Female'])
+st.multiselect('choose a planet',['Jupiter', 'Mars', 'neptune'])
+st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+st.slider('Pick a number', 0,60)
+
+
