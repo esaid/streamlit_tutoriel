a78a9d2ace24130baaebb70109590585835934e2
emmanuel.said <emmanuel.said@cern.ch>
2022-12-14 11:47:29 +0100

    amelioration interface ajout liste port series node in ace creation st.session[folder_project] creation st.session[folder_streamlit]

--- streamlit_ga144.py
+++ streamlit_ga144.py
@@ -14,15 +14,15 @@ from itertools import cycle
 
 # lottie url file
 def load_lottieurl(url: str):
-    r = requests.get(url)
+    r = requests.get ( url )
     if r.status_code != 200:
         return None
-    return r.json()
+    return r.json ()
 
 
 def load_lottiefile(filepath: str):
-    with open(filepath, "r") as f:
-        return json.load(f)
+    with open ( filepath, "r" ) as f:
+        return json.load ( f )
 
 
 # gestion projet
@@ -32,38 +32,49 @@ if 'projet' not in st.session_state:
 # gestion repertoire et fichiers dans projet
 if 'list_files_project' not in st.session_state:
     st.session_state['list_files_project'] = ''
+# gestion repertoire projet
+if 'folder_project' not in st.session_state:
+    st.session_state['folder_project'] = ''
+
+if 'folder_streamlit' not in st.session_state:
+    st.session_state['folder_streamlit'] = "\n\r".join (os.getcwd ().splitlines () )  # sauvegarde repertoire streamlit
 
 # elargir la page
-st.set_page_config(layout="wide")
+st.set_page_config ( layout="wide" )
 # charger animation cpu
 cpu_file = "cpu.json"
-lottie_cpu = load_lottiefile(cpu_file)
+dir_cpu_file = f"{st.session_state['folder_streamlit']}\{cpu_file}"
+os.chdir ( rf"{st.session_state['folder_streamlit']}" )  # path streamlit
+st.write(os.getcwd())
+lottie_cpu = load_lottiefile ( cpu_file )
+
+
 # charger animation ecriture code informatique
 lottie_urlGA144 = "https://assets9.lottiefiles.com/packages/lf20_xafe7wbh.json"
-lottie_jsonGA144 = load_lottieurl(lottie_urlGA144)
+lottie_jsonGA144 = load_lottieurl ( lottie_urlGA144 )
 
 # titre avec style css html
 original_title = '<p style="font-family:Courier; color:Green; font-size: 40px;">GA144 FORTH</p>'
-st.markdown(original_title, unsafe_allow_html=True)
+st.markdown ( original_title, unsafe_allow_html=True )
 # affichage repertoire fichiers du projet si exisant
 if st.session_state['projet'] is True:
-    st.write("Projet :: \n")
+    st.write ( "Projet :: \n" )
     # l = str(st.session_state['list_files_project']).split("\n")
-    for l in str(st.session_state['list_files_project']).split("\n"):
-        st.write(f"\n ---->  {l}")
+    for l in str ( st.session_state['list_files_project'] ).split ( "\n" ):
+        st.write ( f"\n ---->  {l}" )
 # afficher  animation cpu
-with st.spinner(text="GA144"):
-    st_lottie(lottie_jsonGA144, height=150, key="loading_gif")
+with st.spinner ( text="GA144" ):
+    st_lottie ( lottie_jsonGA144, height=150, key="loading_gif" )
 # afficher animation cpu et menu vertical
 with st.sidebar:
-    st_lottie(lottie_cpu, speed=0.2, height=150)
-    selected_vertical_menu = option_menu("Main Menu", ["Home", 'Settings', 'About'], icons=['house', 'gear'],
-                                         menu_icon="cast",
-                                         default_index=0)
+    st_lottie ( lottie_cpu, speed=0.2, height=150 )
+    selected_vertical_menu = option_menu ( "Main Menu", ["Home", 'Settings', 'About'], icons=['house', 'gear'],
+                                           menu_icon="cast",
+                                           default_index=0 )
     # selection node par type et numero node
-    node_type = st.selectbox(
+    node_type = st.selectbox (
         'Node Type Selection :',
-        ('NODE', 'GPIO', 'Analog-In', 'Analog-Out', 'CLK', 'DATA', 'Internal'))
+        ('NODE', 'GPIO', 'Analog-In', 'Analog-Out', 'CLK', 'DATA', 'Internal') )
 
     list_node = ''
     if node_type == 'GPIO':
@@ -78,123 +89,130 @@ with st.sidebar:
         list_node = '300', '001', '701'
     if node_type == 'Internal':
         list_node = '002', '003', '004', '005', '006', '010', '011', '012', '013', '014', '015', '016', '017', '101', '102', '103', '104', '105', '106', '107', \
-                    '108', '109', '110', '111', '112', '113', '114', '115', '116', '201', '202', '203', '204', '205', '206', '207', '208', '209', '210', '211', '212', \
-                    '213', '214', '215', '216', '300', '301', '302', '303', '304', '305', '306', '307', '308', '309', '310', '311', '312', '313', '314', '315', '316', \
-                    '400', '401', '402', '403', '404', '405', '406', '407', '408', '409', '410', '411', '412', '413', '414', '415', '416', \
-                    '501', '502', '503', '504', '505', '506', '507', '508', '509', '510', '511', '512', '513', '514', '515', '516', \
-                    '601', '602', '603', '604', '605', '606', '607', '608', '609', '610', '611', '612', '613', '614', '615', '616', \
-                    '700', '701', '702', '703', '704', '706', '707', '708', '710', '711', '712', '714', '716'
+            '108', '109', '110', '111', '112', '113', '114', '115', '116', '201', '202', '203', '204', '205', '206', '207', '208', '209', '210', '211', '212', \
+            '213', '214', '215', '216', '300', '301', '302', '303', '304', '305', '306', '307', '308', '309', '310', '311', '312', '313', '314', '315', '316', \
+            '400', '401', '402', '403', '404', '405', '406', '407', '408', '409', '410', '411', '412', '413', '414', '415', '416', \
+            '501', '502', '503', '504', '505', '506', '507', '508', '509', '510', '511', '512', '513', '514', '515', '516', \
+            '601', '602', '603', '604', '605', '606', '607', '608', '609', '610', '611', '612', '613', '614', '615', '616', \
+            '700', '701', '702', '703', '704', '706', '707', '708', '710', '711', '712', '714', '716'
 
-    node = st.selectbox('Node', list_node)
-    st.write('NODE Type selected:', node_type)
-    st.write('NODE selected:', node)
+    node = st.selectbox ( 'Node', list_node )
+    st.write ( 'NODE Type selected:', node_type )
+    st.write ( 'NODE selected:', node )
 
 # col2 creation ou col1 ouvrir un projet le fichier ini.ga ( pour connaitre le repertoire )
-col1, col2 = st.columns([1, 1])
+col1, col2 = st.columns ( [1, 1] )
 # charger  projet
 with col1:
-    placeholder_col1 = st.empty()  # permet de faire disparaitre les elements
-    with placeholder_col1.container():
+    placeholder_col1 = st.empty ()  # permet de faire disparaitre les elements
+    with placeholder_col1.container ():
         # si pas de projet , on sélectionne le repertoire et fichier ini.ga
         if st.session_state['projet'] is False:
-            st.title('Load Project :')
-            select_projet = st.file_uploader("Choose a file ini.ga in project folder ", type=['ga'])
+            st.title ( 'Load Project :' )
+            select_projet = st.file_uploader ( "Choose a file ini.ga in project folder ", type=['ga'] )
             if select_projet:
                 st.session_state['projet'] = True
-                directory_project = select_projet.getvalue().decode('utf-8')[2:]  # chemin du projet
+                directory_project = select_projet.getvalue ().decode ( 'utf-8' )[2:]  # chemin du projet
+                st.session_state['folder_project'] = directory_project
                 st.session_state['list_files_project'] += directory_project
                 st.session_state['list_files_project'] += 'ini.ga'  # fichier ini.ga
-                st.write(st.session_state['list_files_project'])
-                time.sleep(4)
-                placeholder_col1.empty().empty()  # on fait disparaitre les elements
+                st.write ( st.session_state['list_files_project'] )
+                time.sleep ( 4 )
+                placeholder_col1.empty ().empty ()  # on fait disparaitre les elements
 # creer projet
 with col2:
-    placeholder_col2 = st.empty()  # permet de faire disparaitre les elements
-    with placeholder_col2.container():
+    placeholder_col2 = st.empty ()  # permet de faire disparaitre les elements
+    with placeholder_col2.container ():
         # si pas de projet  , creation du projet
         if st.session_state['projet'] is False:
-            st.title('Create Project :')
-            cwd = os.getcwd()  # folder courant
-            projet = st.text_input('Project :  👇')  # nom du projet
-            st.write(f"Current working directory: {cwd}")
-            if not projet:  # gere si on a bien rentrer un nom de projet
-                st.warning('Please input a name directory project')
-                st.stop()
-            st.session_state['projet'] = True
-            st.success(f"Thank you for inputting a name. {st.session_state['projet']}")
-            path = os.path.join(cwd, projet)  # chemin repertoire du projet
-            try:
-                os.mkdir(path)  # creation repertoire , avec nom de projet
-            except OSError as errordirectory:
-                st.error(f'This is an error  {errordirectory}', icon="🚨")
-                st.stop()
-            os.chdir(path)  # path projet
-            st.info(f'Create init.ga file in {projet}', icon="ℹ️")
-            init_text = f"/ {path}\n"  #
-            # creation du fichier ini.ga
-            with open('init.ga', "w") as f:
-                f.write(init_text)  # save code init file
+            st.title ( 'Create Project :' )
 
-            os.chdir(cwd)  # path_initial
-            st.session_state['list_files_project'] += f"{path}\n"
+            projet = st.text_input ( 'Project :  👇' )  # nom du projet
+            st.write ( f"Current working directory: {st.session_state['folder_streamlit']}" )  # folder courant
+            if not projet:  # gere si on a bien rentrer un nom de projet
+                st.warning ( 'Please input a name directory project' )
+                st.stop ()
+            st.session_state['projet'] = True
+            st.success ( f"Thank you for inputting a name. {st.session_state['projet']}" )
+            st.session_state['folder_project'] = os.path.join ( st.session_state['folder_streamlit'],
+                                                                projet )  # chemin repertoire du projet
+            try:
+                os.mkdir ( st.session_state['folder_project'] )  # creation repertoire , avec nom de projet
+            except OSError as errordirectory:
+                st.error ( f'This is an error  {errordirectory}', icon="🚨" )
+                st.stop ()
+            os.chdir ( rf"{st.session_state['folder_project']}" )  # path projet
+            st.info ( f'Create init.ga file in {projet}', icon="ℹ️" )
+            init_text = f"/ {st.session_state['folder_project']}\n"  #
+            # creation du fichier ini.ga
+            with open ( 'init.ga', "w" ) as f:
+                f.write ( init_text )  # save code init file
+
+            # os.chdir ( st.session_state['folder_streamlit'] )  # path_initial
+            st.session_state['list_files_project'] += f"{st.session_state['folder_project']}\n"
             st.session_state['list_files_project'] += 'ini.ga'
-            st.write(os.getcwd())
-            time.sleep(5)
-            placeholder_col2.empty().empty()  # clear
-            placeholder_col1.empty().empty()
+            st.write ( os.getcwd () )
+            time.sleep ( 5 )
+            placeholder_col2.empty ().empty ()  # clear
+            placeholder_col1.empty ().empty ()
 # menu horizontal
-selected_horizontal = option_menu(None, ["Home", "New", "Load", 'Save'],
-                                  icons=['house', 'plus-square', 'bi-file-earmark-arrow-down-fill',
-                                         'bi-file-earmark-arrow-up-fill',
-                                         ],
-                                  menu_icon="cast", default_index=0, orientation="horizontal",
-                                  styles={
-                                      "container": {"padding": "0!important", "background-color": "#fafafa"},
-                                      "icon": {"color": "orange", "font-size": "25px"},
-                                      "nav-link": {"font-size": "25px", "text-align": "left", "margin": "0px",
-                                                   "--hover-color": "#eee"},
-                                      "nav-link-selected": {"background-color": "green"},
-                                  }
-                                  )
+selected_horizontal = option_menu ( None, ["Home", "New", "Load", 'Save'],
+                                    icons=['house', 'plus-square', 'bi-file-earmark-arrow-down-fill',
+                                           'bi-file-earmark-arrow-up-fill',
+                                           ],
+                                    menu_icon="cast", default_index=0, orientation="horizontal",
+                                    styles={
+                                        "container": {"padding": "0!important", "background-color": "#fafafa"},
+                                        "icon": {"color": "orange", "font-size": "25px"},
+                                        "nav-link": {"font-size": "25px", "text-align": "left", "margin": "0px",
+                                                     "--hover-color": "#eee"},
+                                        "nav-link-selected": {"background-color": "green"},
+                                    }
+                                    )
 selected_node = []
 data_code = ""
 # charger fichier *.ga
 if selected_horizontal == 'Load':
-    loaded_file = st.file_uploader("Choose a file", type='ga')
+    loaded_file = st.file_uploader ( "Choose a file", type='ga' )
     if loaded_file:
         # bytes_data = loaded_file.getvalue()
-        data_code = loaded_file.getvalue().decode('utf-8')
+        data_code = loaded_file.getvalue ().decode ( 'utf-8' )
         # affiche le code dans editeur ace
-        code_editeur = st_ace(value=data_code, language='forth', theme='cobalt', font_size=25, key=loaded_file.name)
+        code_editeur = st_ace ( value=data_code, language='forth', theme='cobalt', font_size=25, key=loaded_file.name )
 
 if selected_horizontal == 'Save':
-    saved_file = st.file_uploader("Choose a file")
+    saved_file = st.file_uploader ( "Choose a file" )
     saved_file = data_code
 
 if selected_horizontal == 'New':
-    code_editeur = st_ace(value=f"node {node}\n", language='forth', theme='cobalt', font_size=25, key=f"{node}.ga")
+    code_editeur = st_ace ( value=f"node {node}\n", language='forth', theme='cobalt', font_size=25, key=f"{node}.ga" )
     if st.button:  # CTRL + ENTER
-        node_file = f"{code_editeur.title().split()[1]}.ga"  # ['Node','117']  '117.ga'
-        st.text(node_file)
-        # st.code(code_editeur , language='fortran')
-        with open(node_file, "w") as f:
-            f.write(code_editeur)  # save code to '117.ga'
+        node_file = f"{code_editeur.title ().split ()[1]}.ga"  # ['Node','117']  '117.ga'
+        folder_file = f"{st.session_state['folder_project']}\{node_file}".strip ()
+        st.text ( f"Node : {folder_file}" )
+        time.sleep ( 5 )
+        dir = "\n\r".join ( str ( st.session_state['folder_project'] ).splitlines () )
+        st.write ( dir )
+        os.chdir ( dir )  # path projet
+        # os.chdir ( st.session_state['folder_project'] )
+        with open ( node_file, "w" ) as f:  # sauvegarde dans le repertoire projet le fichier node.ga
+            f.write ( code_editeur )  # save code to '117.ga'
 
 if selected_vertical_menu == 'About':
-    st.info('informational message GA144 program ', icon="ℹ️")
+    st.info ( 'informational message GA144 program ', icon="ℹ️" )
 
 # gestion port serie
 if selected_vertical_menu == 'Settings':
     message = ''
-    ports = serial.tools.list_ports.comports()
+    ports = serial.tools.list_ports.comports ()
     list_port = []
-    for port, desc, hwid in sorted(ports):
-        list_port.append(port)
-    option_port_serial = st.selectbox('Serial Port selection', list_port)
-    st.write('You selected:', option_port_serial)
+    for port, desc, hwid in sorted ( ports ):
+        list_port.append ( port )
+    option_port_serial = st.selectbox ( 'Serial Port selection', list_port )
+    st.write ( 'You selected:', option_port_serial )
 
 # gestion GA144 nodes
-my_expander = st.expander(label='GA144 Nodes')
+my_expander = st.expander ( label='GA144 Nodes' )
 with my_expander:
     list_node_button = [
 
@@ -216,6 +234,6 @@ with my_expander:
         "016", "017"
     ]
 
-    cols = cycle(st.columns(18))  # st.columns here since it is out of beta at the time I'm writing this
-    for idx, button_node in enumerate(list_node_button):
-        next(cols).button(label=str(button_node))
+    cols = cycle ( st.columns ( 18 ) )  # st.columns here since it is out of beta at the time I'm writing this
+    for idx, button_node in enumerate ( list_node_button ):
+        next ( cols ).button ( label=str ( button_node ) )
