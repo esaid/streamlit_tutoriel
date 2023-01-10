a5e7fa9b6c96597e18da6d1da614ac2db46f5151
esaid <emmanuel.said@cern.ch>
2023-01-10 09:58:51 +0100

    tutoriel integrer des animations utilisation du fichier json lottie.com slidebar changment couleurs theme fichier config.toml option_menu ace file node type selection et node selection load file grid button save code  in file create project os.chdir clear session_state pour gerer projet placeholder empty repertoire projet affichage projet ajout commentaire session_state name projet trie liste fichiers select_projet = st.file_uploader # lottie_cpu = load_lottiefile(cpu_file) save avec st.session.state view_code_node new test node select_folder_project file_projetct_font suppression st.write(folder ect) qui ne sont pas necessaire st.session_state['serial_port'] new project path folder principal master_folder st.session_state['folder_streamlit'] = os.path.dirname(__file__) folder_principal node en couleur gestion GA144 nodes help fonction nodes tuples menu compilation send bar_progression metric split a la place de splitlines pour gerer les repertoires my_bar.empty() Lib suppression choix Lib simplification menu projet new st.stop() hide col1 col2 Load / new projet file Cga compilation modification save load viewcode stdout directory_project = select_projet.getvalue().decode('utf-8').split('\n')[0][2:] compilation expander importation bibliotheque_create.py gacompilation send tester port serie remplace extension .ga par .node requirement.txt login passwords database authentification

--- database.py
+++ database.py
@@ -1,33 +1,81 @@
 import os
+import sys
 from deta import Deta
-from dotenv import load_dotenv
+from dotenv import load_dotenv  # python-dotenv
 import pickle
 import streamlit_authenticator as stauth
 
+# --------------------------------------------------
 # initialisation database DETA_KEY
 load_dotenv(".env")  # la DETA_KEY est cache
 DETA_KEY = os.getenv("DETA_KEY")
 deta = Deta(DETA_KEY)
 drive = deta.Drive("simple_drive")
+# --------------------------------------------------
+
+# --------------------------------------------------
+# initialisation Base et Drive
+# Base pour user , project , lien commun username
+db_user = deta.Base('user')  # users database
+db_project = deta.Base('project')  # project database
+# Drive pour avatar , lib, project_name ( concatenation username_name_project )
+
+path_avatar_drive = 'avatar'  # fichiers stocke avatar/001.png
+avatar_drive = deta.Drive(path_avatar_drive)  # avatar/ Drive
+path_lib_drive = 'lib'
+lib_drive = deta.Drive(path_lib_drive)  # lib/ Drive
+
+db_hashed = deta.Drive('hashed_pwd.plk')  # hashed Drive
+
+def next_key(d_):
+    return max(list(map(int, filter_database(d_,'key'))))+1
+# --------------------------------------------------
+def autentificator_list_dict(list_usernames_, list_email_, list_name_, list_passwords_, list_emails_prehautorized_,
+                             list_value_cookies_):
+    list_user = ["email", "name", "password"]
+    list_cookies = ["expiry_days", "key", "name"]
+    list_value_prehautorized = {"emails": list_emails_prehautorized_}
+
+    # generation user list
+    l_user_values = []
+    for n in range(len(list_usernames_)):
+        l_user_values.append([list_email_[n], list_name_[n], list_passwords_[n]])
+
+    # list to dict
+    credentials = {}
+    usernames = {}
+    cookie = {'cookie': dict(zip(list_cookies, list_value_cookies_))}
+    prehautorized = {'preauthorized': list_value_prehautorized}
+
+    user_values = {}
+    for n in range(len(list_usernames_)):
+        usernames[list_usernames_[n]] = dict(zip(list_user, l_user_values[n]))
+
+    usernames = {'usernames': usernames}
+    config = {'credentials': usernames, **cookie, **prehautorized}  # merge dict
+    return config
 
 
-# generation hashed_passwords et sauvegrde dans le fichier hashed_pwd.plk
-def generate_hashed_passwords(name, username, password_):
+# generation hashed_passwords et sauvegrde dans le fichier file_
+def generate_hashed_passwords_file(name, username, password_, file_):
     hashed_passwords = stauth.Hasher(password_).generate()
-
-    # print(f"generation password :{hashed_passwords}")
+    print(f"generation password :{hashed_passwords}")
     # ecriture du fichier passwords
     # print("ecriture fichier :")
-    with open('hashed_pwd.plk', 'wb') as f:
+    with open(file_, 'wb') as f:
         pickle.dump(name, f)
         pickle.dump(username, f)
         pickle.dump(hashed_passwords, f)
         f.close()
 
+def generate_hashed_passwords(password_):
+    return stauth.Hasher(password_).generate()
+
 
 def read_hashed_passwords(file_):
     # lecture fichier hashed_pwd.plk
     with open(file_, 'rb') as f:
+        name_ = pickle.loads(f)
         name_ = pickle.load(f)
         username_ = pickle.load(f)
         password_ = pickle.load(f)
@@ -35,25 +83,82 @@ def read_hashed_passwords(file_):
         return name_, username_, password_
 
 
-def get_file_drive(name_drive, file_):
-    d = deta.Drive(name_drive)
-    get_d = d.get(f"{file_}")
+
+
+
+def readfile(filename):
+    with open(filename) as f:
+        content = f.readlines()
+        return content
+
+
+def put_database(database_, dict_):
+    database_.put(dict_)
+
+
+def fetch_all(database_):
+    res = database_.fetch()
+    return res.items
+
+
+def get_database(database_, key_):
+    return database_.get(key_)
+
+
+def filter_database(database_items_, key_):
+    l_ = []
+    for l in database_items_:
+        l_.append(l[key_])
+    return l_
+
+
+def fetch_database(database_, query_):  # fetch_database(db_user, {"name_project": 'led', "name": 'toto'} )
+    res = database_.fetch(query_)
+    res =res.items
+    return res
+
+
+def update_database(database_, update_values_, key_):
+    d = get_database(database_, key_)  # lecture {}
+    d.update(update_values_)  # update {}
+    database_.put(d)
+
+
+def delete_database(database_, query_):  # delete_database(db_user, {"name": "Emmanuel"})
+    fetch_res = database_.fetch(query_)
+    for item in fetch_res.items:
+        db_user.delete(item["key"])
+
+
+def get_code(getname_node, db):
+    return db.get(getname_node)['code']
+
+
+def get_file_drive(drive_, file_):
+    get_d = drive_.get(f"{file_}")
     content = get_d.read()
     get_d.close()
     return content
 
 
-def put_file_drive(name_drive, file_, path_local):
-    d = deta.Drive(name_drive)
-    d.put(f"{file_}", path=f"{path_local}/{file_}")
+def put_file_drive(drive_, file_, path_local):
+    drive_.put(f"{file_}", path=f"{path_local}{file_}")
 
 
-def delete_file_drive(name_drive, file_):
-    d = deta.Drive(name_drive)
-    d.delete(f"{file_}")
+def delete_file_drive(drive_, file_):
+    drive_.delete(f"{file_}")
 
 
-def list_files(name_drive):
-    d = deta.Drive(name_drive)
-    return d.list().get('names')
+def list_files(drive_):
+    return drive_.list().get('names')
 
+
+
+# a = fetch_projet(db_user, {"name_project": 'led', "name": 'toto'})
+# sys.exit()
+
+# name_ = 'Emmanuel'
+# a = get_database(db_user, '1')
+# print(a)
+# update_database(db_user, {'name': name_}, '1')
+# sys.exit()
--- streamlit_ga144.py
+++ streamlit_ga144.py
@@ -19,33 +19,80 @@ import sys
 import subprocess
 import traceback
 
-
 # élargir la page
 st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
+path_avatar_drive = 'avatar'
+if "avatar" not in st.session_state:
+    st.session_state["avatar"] = ''
 
-# Authentification
-#name = ["admin", "emmanuel said"]
-#username = ["admin", "esaid"]
-#passwords = ["1234", "1234"]  # 1234  1234
+if 'login' not in st.session_state:
+    st.session_state["login"] = False
+# read datas from user.database
+d = database.fetch_all(database.db_user)  # all items user database
+next_key = database.next_key(d)
 
-# generation fichier si ajout ou modification mot de passe
-# database.generate_hashed_passwords( name, username, passwords)
+list_usernames = database.filter_database(d, "username")  # all values username
+list_name = database.filter_database(d, "name")  # all values name
+list_email = database.filter_database(d, "email")  # all values email
+list_passwords = database.filter_database(d, "password")  # values password
+list_avatar = database.filter_database(d, 'avatar')  # values avatar
+list_emails_prehautorized = ["emmanuel.said@gmail.com"]
+list_value_cookies = [30, "random_signature_key", "random_cookie_name"]
+# read list_passwords ( hashed values)
+# list_name, list_usernames, list_passwords = database.read_hashed_passwords('hashed_pwd.plk')
 
-name, username, hashed_passwords = database.read_hashed_passwords('hashed_pwd.plk')
-# print(f"lecture fichier et decodage {hashed_passwords}")
-authenticator = stauth.Authenticate(name, username, hashed_passwords, 'some_cookie_name', 'some_signature_key',
-                                    cookie_expiry_days=30)
+config = database.autentificator_list_dict(list_usernames, list_email, list_name, list_passwords,
+                                           list_emails_prehautorized, list_value_cookies)
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
 name, authentication_status, username = authenticator.login('GA144', 'main')
-if authentication_status :
-    st.write(f'Welcome *{username}*')
-    authenticator.logout('Logout', 'main')
-    time.sleep(1)
-elif authentication_status == False:
-    st.error('Username/password is incorrect')
-    st.stop()
-elif authentication_status == None:
-    st.warning('Please enter your username and password')
-    st.stop()
+
+placeholder = st.empty()
+with placeholder.container():
+    if authentication_status:
+        st.session_state["login"] = True
+        st.session_state["avatar"] = list_avatar[list_usernames.index(f"{username}")]
+        st.image(database.get_file_drive(database.avatar_drive, st.session_state["avatar"]), width=70,
+                 caption=f"Welcome {username}")
+        authenticator.logout('Logout', 'main')
+        time.sleep(2)
+    elif authentication_status == False:
+        st.error('Username/password is incorrect')
+    elif authentication_status == None:
+        st.warning('Please enter your username and password')
+    if st.session_state["login"] == False:
+        try:
+            st.session_state["avatar"] = st.selectbox("my avatar 👇", database.list_files(database.avatar_drive))
+            st.image(database.get_file_drive(database.avatar_drive, st.session_state["avatar"]), width=70)
+            if authenticator.register_user('Register user', preauthorization=False):
+                st.success('User registered successfully')
+                # st.write(config) # recueration des valeurs register_user
+                last_username = list(config['credentials']['usernames'])[-1] # last register
+                last_name = config['credentials']['usernames'][last_username]['name']
+                last_email = config['credentials']['usernames'][last_username]['email']
+                last_password = config['credentials']['usernames'][last_username]['password']
+                dict_db_user = {
+                    'name': last_name,
+                    'username': last_username,
+                    'email': last_email,
+                    'avatar': st.session_state["avatar"],
+                    'password': last_password,
+                    'key': str(next_key)
+                }
+                # st.write(dict_db_user)
+                # st.write(last_username, last_name, last_email, last_password)
+                database.put_database(database.db_user, dict_db_user)  # ecriture dans datatbase user
+                time.sleep(2)
+                st.session_state["login"] = True
+                placeholder.empty()
+        except Exception as e:
+            st.error(e)
 
 
 
